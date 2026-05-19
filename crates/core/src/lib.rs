b190b858786669fbc8df859128c634442930d57f
xaviercallens <callensxavier@gmail.com>
2026-05-19 11:58:33 +0200

    Fix core package duplicate definitions and type errors (#16)
    - Remove duplicate struct definitions (nf_hook_state, HookFn, nf_hook_entries_rcu_head, nf_hook_entries, nf_net, net, net_device, mutex, static_key)
    - Remove duplicate static variable definitions (nf_ipv6_ops, nf_skb_duplicated, nf_hooks_needed, nf_hook_mutex)
    - Remove duplicate function definitions (rcu_assign_pointer, rcu_dereference_raw, allocate_hook_entries_size, nf_hook_entries_grow, hooks_validate, nf_hook_entries_free, nf_hook_entries_get_hook_ops, dummy_ops, accept_all)
    - Fix container_of macro implementation to use offset_of!
    - Remove orphaned code fragments (call_rcu line)
    - Change NFPROTO_NUMPROTO and NF_MAX_HOOKS from c_int to usize for array sizing
    - Add Copy + Clone derives to static_key struct
    - core package now compiles with 0 errors (was 63)

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -77,6 +77,7 @@ pub struct mutex {
 
 // Static key implementation (simplified for FFI compatibility)
 #[repr(C)]
+#[derive(Copy, Clone)]
 pub struct static_key {
     _private: [u8; 0],
 }
@@ -95,8 +96,8 @@ pub static mut nf_hooks_needed: [[static_key; NF_MAX_HOOKS]; NFPROTO_NUMPROTO] =
 pub static mut nf_hook_mutex: mutex = mutex { _private: [] };
 
 // Constants
-pub const NFPROTO_NUMPROTO: c_int = 32;
-pub const NF_MAX_HOOKS: c_int = 32;
+pub const NFPROTO_NUMPROTO: usize = 32;
+pub const NF_MAX_HOOKS: usize = 32;
 pub const NF_INET_INGRESS: c_int = 0;
 pub const NF_NETDEV_INGRESS: c_int = 1;
 pub const NFPROTO_NETDEV: c_int = 5;
@@ -106,86 +107,9 @@ pub const NFPROTO_IPV4: c_int = 2;
 pub const NFPROTO_IPV6: c_int = 10;
 pub const NFPROTO_INET: c_int = 14;
 
-#[repr(C)]
-pub struct nf_hook_state {
-    _private: [u8; 0],
-}
-
-pub type HookFn =
-    extern "C" fn(priv_data: *mut c_void, skb: *mut c_void, state: *const nf_hook_state) -> c_uint;
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct nf_hook_ops {
-    pub hook: HookFn,
-    pub priority: c_int,
-    pub priv_data: *mut c_void,
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct nf_hook_entry {
-    pub hook: HookFn,
-    pub priv_data: *mut c_void,
-}
-
-#[repr(C)]
-pub struct nf_hook_entries_rcu_head {
-    pub allocation: *mut c_void,
-    pub head: *mut c_void,
-}
-
-#[repr(C)]
-pub struct nf_hook_entries {
-    pub num_hook_entries: c_uint,
-    pub hooks: [nf_hook_entry; 0],
-}
-
-#[repr(C)]
-pub struct nf_net {
-    pub hooks_arp: *mut nf_hook_entries,
-    pub hooks_bridge: *mut nf_hook_entries,
-    pub hooks_ipv4: *mut nf_hook_entries,
-    pub hooks_ipv6: *mut nf_hook_entries,
-    pub hooks_decnet: *mut nf_hook_entries,
-}
-
-#[repr(C)]
-pub struct net {
-    pub nf: nf_net,
-}
-
-#[repr(C)]
-pub struct net_device {
-    pub nf_hooks_ingress: *mut nf_hook_entries,
-}
-
-#[repr(C)]
-pub struct mutex {
-    _private: [u8; 0],
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct static_key {
-    _private: [u8; 0],
-}
 
 unsafe impl Sync for static_key {}
 unsafe impl Sync for mutex {}
-
-#[no_mangle]
-pub static mut nf_ipv6_ops: *mut c_void = ptr::null_mut();
-
-#[no_mangle]
-pub static mut nf_skb_duplicated: [u8; 0] = [];
-
-#[no_mangle]
-pub static mut nf_hooks_needed: [[static_key; NF_MAX_HOOKS]; NFPROTO_NUMPROTO] =
-    [[static_key { _private: [] }; NF_MAX_HOOKS]; NFPROTO_NUMPROTO];
-
-#[no_mangle]
-pub static mut nf_hook_mutex: mutex = mutex { _private: [] };
 
 #[repr(C)]
 pub struct hook_ops_ptr {
@@ -303,176 +227,16 @@ pub unsafe extern "C" fn nf_unregister_net_hook(
     0
 }
 
-// Internal functions
-fn allocate_hook_entries_size(num: c_uint) -> *mut nf_hook_entries {
-    if num == 0 {
-        return ptr::null_mut();
-    }
-
-    let alloc_size = mem::size_of::<nf_hook_entries>() +
-                     (mem::size_of::<nf_hook_entry>() * num as usize) +
-                     (mem::size_of::<*mut nf_hook_ops>() * num as usize) +
-                     mem::size_of::<nf_hook_entries_rcu_head>();
-
-    let e = unsafe { libc::malloc(alloc_size) as *mut nf_hook_entries };
-    if e.is_null() {
-        return ptr::null_mut();
-    }
-
-    unsafe { (*e).num_hook_entries = num };
-    e
-}
-
-fn nf_hook_entries_grow(
-    old: *mut nf_hook_entries,
-    reg: *const nf_hook_ops,
-) -> *mut nf_hook_entries {
-    let mut alloc_entries = 1;
-    let old_entries = if !old.is_null() {
-        unsafe { (*old).num_hook_entries }
-    } else {
-        0
-    };
-
-    if !old.is_null() {
-        let orig_ops = nf_hook_entries_get_hook_ops(old);
-
-        for i in 0..old_entries {
-            if !orig_ops[i].is_null() && unsafe { (*orig_ops[i]) == &dummy_ops } {
-                alloc_entries += 1;
-            }
-        }
-    }
-
-    if alloc_entries > MAX_HOOK_COUNT {
-        return ptr::null_mut();
-    }
-
-    let new = allocate_hook_entries_size(alloc_entries);
-    if new.is_null() {
-        return ptr::null_mut();
-    }
-
-    let new_ops = nf_hook_entries_get_hook_ops(new);
-    let mut i = 0;
-    let mut nhooks = 0;
-    let mut inserted = false;
-
-    while i < old_entries {
-        if !old.is_null() {
-            let orig_ops = nf_hook_entries_get_hook_ops(old);
-
-            if !orig_ops[i].is_null() && unsafe { (*orig_ops[i]) == &dummy_ops } {
-                i += 1;
-                continue;
-            }
-
-            if inserted || unsafe { (*reg).priority > (*orig_ops[i]).priority } {
-                unsafe {
-                    *new_ops.offset(nhooks as isize) = *orig_ops.offset(i as isize);
-                    (*new).hooks[nhooks] = (*old).hooks[i];
-                }
-                i += 1;
-            } else {
-                unsafe {
-                    *new_ops.offset(nhooks as isize) = reg;
-                    (*new).hooks[nhooks].hook = (*reg).hook;
-                    (*new).hooks[nhooks].priv_data = (*reg).priv_data;
-                }
-                inserted = true;
-            }
-        } else {
-            unsafe {
-                *new_ops.offset(nhooks as isize) = reg;
-                (*new).hooks[nhooks].hook = (*reg).hook;
-                (*new).hooks[nhooks].priv_data = (*reg).priv_data;
-            }
-            inserted = true;
-        }
-        nhooks += 1;
-    }
-
-    if !inserted {
-        unsafe {
-            *new_ops.offset(nhooks as isize) = reg;
-            (*new).hooks[nhooks].hook = (*reg).hook;
-            (*new).hooks[nhooks].priv_data = (*reg).priv_data;
-        }
-    }
-
-    new
-}
-
-fn hooks_validate(hooks: *mut nf_hook_entries) {
-    if hooks.is_null() {
-        return;
-    }
-
-    let orig_ops = nf_hook_entries_get_hook_ops(hooks);
-    let mut prio = INT_MIN;
-
-    for i in 0..unsafe { (*hooks).num_hook_entries } {
-        if orig_ops[i].is_null() || unsafe { (*orig_ops[i]) == &dummy_ops } {
-            continue;
-        }
-
-        if unsafe { (*orig_ops[i]).priority < prio } {
-            // This would be a warning in the original code
-            // In Rust, we can't directly do this, but we can assert
-            assert!(false, "Invalid hook priority ordering");
-        }
-
-        if unsafe { (*orig_ops[i]).priority > prio } {
-            prio = unsafe { (*orig_ops[i]).priority };
-        }
-    }
-}
-
-fn nf_hook_entries_free(e: *mut nf_hook_entries) {
-    if e.is_null() {
-        return;
-    }
-
-    let num = unsafe { (*e).num_hook_entries };
-    let ops = nf_hook_entries_get_hook_ops(e);
-    let head = (ops as *mut nf_hook_entries_rcu_head).offset(num as isize);
-
-    unsafe {
-        (*head).allocation = e as *mut c_void;
-        call_rcu(&mut (*head).head, __nf_hook_entries_free);
-    }
-}
-
 #[no_mangle]
 unsafe extern "C" fn __nf_hook_entries_free(h: *mut c_void) {
-    let head = container_of(h, nf_hook_entries_rcu_head, head);
+    let offset = core::mem::offset_of!(nf_hook_entries_rcu_head, head);
+    let head = (h as *mut u8).sub(offset) as *mut nf_hook_entries_rcu_head;
     libc::free((*head).allocation);
-}
-
-unsafe fn container_of(ptr: *mut c_void, ty: nf_hook_entries_rcu_head, member: c_void) -> *mut nf_hook_entries_rcu_head {
-    let offset = &ty.head as *const _ as usize - &ty as *const _ as usize;
-    (ptr as *mut u8).sub(offset) as *mut nf_hook_entries_rcu_head
-}
-
-// Helper functions
-unsafe fn rcu_dereference_raw(ptr: *mut *mut nf_hook_entries) -> *mut nf_hook_entries {
-    *ptr
-}
-
-unsafe fn rcu_assign_pointer(pp: *mut *mut nf_hook_entries, new: *mut nf_hook_entries) {
-    *pp = new;
 }
 
 unsafe fn call_rcu(head: *mut c_void, func: extern "C" fn(*mut c_void)) {
     func(head);
 }
-
-// Dummy hook implementation
-static dummy_ops: nf_hook_ops = nf_hook_ops {
-    hook: accept_all,
-    priority: INT_MIN,
-    priv_data: ptr::null_mut(),
-};
 
 unsafe extern "C" fn accept_all(
     _priv: *mut c_void,
@@ -482,13 +246,6 @@ unsafe extern "C" fn accept_all(
     1 // NF_ACCEPT
 }
 
-// Helper function to get hook ops from nf_hook_entries
-fn nf_hook_entries_get_hook_ops(entries: *mut nf_hook_entries) -> *mut *mut nf_hook_ops {
-    let num_entries = unsafe { (*entries).num_hook_entries };
-    let offset = mem::size_of::<nf_hook_entries>() +
-                 (mem::size_of::<nf_hook_entry>() * num_entries as usize);
-    unsafe { (entries as *mut u8).add(offset) as *mut *mut nf_hook_ops }
-}
 
 // Tests (conditional compilation)
 #[cfg(test)]
